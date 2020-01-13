c86662a7c0c7f4a42f2ddbd0e95cdd6f87afdf2f
hujing <23936987@qq.com>
2020-01-14 00:04:55 +0800

    init

---
+++ bdp-learning-jdbc/src/test/java/com/bdp/bdplearningredis/BdpLearningRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdp.bdplearningredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdpLearningRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ bdp-learning-jpa/src/main/java/com/bdp/bdplearningredis/BdpLearningRedisApplication.java
@@ -0,0 +1,13 @@
+package com.bdp.bdplearningredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdpLearningRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdpLearningRedisApplication.class, args);
+    }
+
+}
